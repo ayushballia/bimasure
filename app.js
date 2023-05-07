e424fd2cf662f01d9af23e2f8259801d311e0238
Ayush Tiwari <ayushtiwari@live.in>
2023-05-08 05:22:30 +0530

    update

--- app.js
+++ app.js
@@ -8,3 +8,32 @@ const textLoad = () => {
 
 textLoad();
 setInterval(textLoad, 12000);
+
+
+function dropdownClick() {
+    document.getElementById("myDropdown").classList.toggle("show");
+
+    window.onclick = function(event) {
+        if(!event.target.matches('.dropbtn')){
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i ;
+            for(i = 0; i<dropdowns.length; i++){
+                var opendropdown = dropdowns[i];
+                if (opendropdown.classList.contains("show")) {
+                    opendropdown.classList.remove("show");
+                }
+            }
+        }
+    }
+}
+
+$(document).ready(function(){
+    $(".dropdown").mouseenter(function(){
+        $(".dropdown-content").addClass("show")
+        console.log("mouse enter")
+    })
+    $(".dropdown").mouseleave(function(){
+        $(".dropdown-content").removeClass("show")
+        console.log("mouse enter")
+    })
+})
