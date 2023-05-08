d6f186729481074e98692855afe9e7d072cfbc5e
Ayush Tiwari <ayushtiwari@live.in>
2023-05-08 22:51:21 +0530

    update

--- app.js
+++ app.js
@@ -37,3 +37,41 @@ $(document).ready(function(){
         console.log("mouse enter")
     })
 })
+
+const menuBtn = document.querySelector(".menu-btn");
+const menuItems = document.querySelector(".menu-items");
+const menuItem = document.querySelectorAll('.menu-item');
+// main toggle
+menuBtn.addEventListener("click", () => {
+    toggle();
+});
+
+// toggle on item click it open
+menuItem.forEach(item => {
+    item.addEventListener('click', () => {
+        if (menuBtn.classList.contains('open')) {
+            toggle();
+        }
+    });
+});
+
+function toggle() {
+    menuBtn.classList.toggle("open");
+    menuItems.classList.toggle("open");
+};
+
+function readmore(){
+    var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("readmore");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more..."; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
